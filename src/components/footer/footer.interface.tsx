8b2b538b019da7af78e5c8fa191becae05d5de21
Alex Mena <amena@lukapay.io>
2023-01-10 12:46:23 -0400

    Eslint

--- src/components/footer/footer.interface.tsx
+++ src/components/footer/footer.interface.tsx
@@ -1,20 +1,18 @@
-import { ChangeEventHandler, Dispatch, SetStateAction } from "react"
-
 export interface OptionsProps {
-    label: string
-    value: string
+  label: string
+  value: string
 }
 
 export interface FooterBoxProps {
-    title: boolean
-    text: string
+  title: boolean
+  text: string
 }
 
 export interface FooterListProps {
-    texts: FooterBoxProps[]
+  texts: FooterBoxProps[]
 }
 
 export interface FooterSelectProps {
-    options: Array<OptionsProps>
-    value: string
+  options: OptionsProps[]
+  value: string
 }
