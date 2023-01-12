1dd4daea84f2dda080faa4d464916f513ecf0e8d
Ricardo <97.rramirez@gmail.com>
2023-01-11 21:19:45 -0300

    se reemplaza icono location

--- src/components/footer/FooterSelect/index.tsx
+++ src/components/footer/FooterSelect/index.tsx
@@ -1,34 +1,30 @@
 import { Box, TextField, MenuItem } from '@mui/material'
 import { FooterSelectProps } from '../footer.interface'
 import InputAdornment from '@mui/material/InputAdornment'
-import LocationOnIcon from '@mui/icons-material/LocationOn'
+import Location from 'assets/footer/location.png'
 
 const FooterSelect = ({ options, value }: FooterSelectProps): JSX.Element => {
   return (
     <Box
-      sx={
-          {
-            width: '180px'
-          }
-        }
+      sx={{
+        width: '180px'
+      }}
     >
       <TextField
         fullWidth
         select
         value={value}
         variant={'standard'}
-        InputProps={
-          {
-            startAdornment: (
-              <InputAdornment position="start">
-                <LocationOnIcon />
-              </InputAdornment>
-            ),
-            disableUnderline: true
-          }
-        }
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <img src={Location} alt="flag" />
+            </InputAdornment>
+          ),
+          disableUnderline: true
+        }}
       >
-        {options.map((option) => (
+        {options.map(option => (
           <MenuItem key={option.value} value={option.value}>
             {option.label}
           </MenuItem>
