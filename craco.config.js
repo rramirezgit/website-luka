4c13baec739e8b6ea4e6e679ef7f82feed102460
Ricardo <97.rramirez@gmail.com>
2023-02-10 17:04:59 -0300

    fix

---
+++ craco.config.js
@@ -0,0 +1,27 @@
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+
+module.exports = {
+  webpack: {
+    configure: webpackConfig => {
+      webpackConfig.optimization.minimize = true
+      webpackConfig.optimization.minimizer.push(
+        new ImageMinimizerPlugin({
+          minimizer: {
+            implementation: ImageMinimizerPlugin.imageminMinify,
+            options: {
+              plugins: [['mozjpeg', { quality: 85 }]]
+            }
+          },
+          generator: {
+            preset: 'webp',
+            implementation: ImageMinimizerPlugin.imageminGenerate,
+            options: {
+              plugins: ['imagemin-webp']
+            }
+          }
+        })
+      )
+      return webpackConfig
+    }
+  }
+}
--- src/components/demo/mobileContent/index.tsx
+++ src/components/demo/mobileContent/index.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable multiline-ternary */
 import { Box, Button } from '@mui/material'
 import Typography from '@mui/material/Typography'
-import linkPhone from 'assets/demoLink/demo-link-phone.svg'
-import gatewayPhone from 'assets/demoLink/demo-gateway-phone.svg'
+import linkPhone from 'assets/demoLink/demo-link-phone.svg?as=webp'
+import gatewayPhone from 'assets/demoLink/demo-gateway-phone.svg?as=webp'
 import placeholder from 'assets/placeholder.svg'
 import { getCssVar } from 'theme'
 import styles from './mobilecontent.module.css'
@@ -21,19 +22,15 @@ const MobileContent = ({ type, onLoad }: Props): JSX.Element => {
   const demo = useSelector((state: RootState) => state.demo)
   return (
     <Box
-      sx={
-        {
-          position: 'relative'
-        }
-      }
+      sx={{
+        position: 'relative'
+      }}
     >
-      <Box
-        id={'mobilecontent-container'}
-      >
+      <Box id={'mobilecontent-container'}>
         <img
-          loading='lazy'
-          src={ type === 'link' ? linkPhone : gatewayPhone }
-          alt='Phone'
+          loading="lazy"
+          src={type === 'link' ? linkPhone : gatewayPhone}
+          alt="Phone"
           style={{ height: '100%' }}
           onLoad={() => onLoad()}
         />
@@ -41,130 +38,126 @@ const MobileContent = ({ type, onLoad }: Props): JSX.Element => {
       <Box
         className={styles['custom-view']}
         id={'mobilecontent-custom-container'}
-        sx={
-          {
-            height: '50%',
-            boxShadow: '0px -3.3448px 6.68961px rgba(0, 0, 0, 0.12)'
-          }
-        }
+        sx={{
+          height: '50%',
+          boxShadow: '0px -3.3448px 6.68961px rgba(0, 0, 0, 0.12)'
+        }}
       >
         <Box
           className={styles.close}
-          sx={
-            {
-              margin: '10px 8px 8px 8px'
-            }
-          }
+          sx={{
+            margin: '10px 8px 8px 8px'
+          }}
         >
           <CloseIcon
             id={'mobilecontent-close'}
-            sx={
-              {
-                color: getCssVar('--dark-gray-text')
-              }
-            }
+            sx={{
+              color: getCssVar('--dark-gray-text')
+            }}
           />
         </Box>
-        <Box
-          className={styles['text-box']}
-        >
-          {
-            type === 'link'
-              ? <Box
-                  sx={
-                    {
-                      display: 'flex',
-                      alignItems: 'flex-start'
-                    }
-                  }
+        <Box className={styles['text-box']}>
+          {type === 'link' ? (
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'flex-start'
+              }}
+            >
+              <Box
+                component={'figure'}
+                id={'mobilecontent-logo'}
+                className={styles.logo}
+              >
+                <img
+                  src={demo.imgUrl ? demo.imgUrl : placeholder}
+                  alt="Logo"
+                  style={{ maxWidth: '100%', maxHeight: '100%' }}
+                />
+              </Box>
+              <Box>
+                <Typography
+                  id={'mobilecontent-business'}
+                  sx={{
+                    fontWeigth: '400 !important',
+                    color: getCssVar('--demo-mobile-gray'),
+                    fontFamily: demo.font?.label
+                      ? `${demo.font?.label} !important`
+                      : 'Open Sans'
+                  }}
                 >
-                <Box
-                  component={'figure'}
-                  id={'mobilecontent-logo'}
-                  className={styles.logo}
+                  {demo.language?.value === 'EN' ? 'My business' : 'Mi negocio'}
+                </Typography>
+                <Typography
+                  id={'mobilecontent-receipt'}
+                  sx={{
+                    fontWeigth: '400 !important',
+                    color: getCssVar('--demo-mobile-gray-l'),
+                    fontFamily: demo.font?.label
+                      ? `${demo.font?.label} !important`
+                      : 'Open Sans'
+                  }}
                 >
-                  <img src={demo.imgUrl ? demo.imgUrl : placeholder} alt='Logo' style={{ maxWidth: '100%', maxHeight: '100%' }} />
-                </Box>
-                <Box>
-                  <Typography
-                    id={'mobilecontent-business'}
-                    sx={
-                      {
-                        fontWeigth: '400 !important',
-                        color: getCssVar('--demo-mobile-gray'),
-                        fontFamily: demo.font?.label ? `${demo.font?.label} !important` : 'Open Sans'
-                      }
-                    }
-                  >
-                    {demo.language?.value === 'EN' ? 'My business' : 'Mi negocio'}
-                  </Typography>
-                  <Typography
-                    id={'mobilecontent-receipt'}
-                    sx={
-                      {
-                        fontWeigth: '400 !important',
-                        color: getCssVar('--demo-mobile-gray-l'),
-                        fontFamily: demo.font?.label ? `${demo.font?.label} !important` : 'Open Sans'
-                      }
-                    }
-                  >
-                    {demo.language?.value === 'EN' ? 'Receipt 123' : 'Recibo 123'}
-                  </Typography>
-                </Box>
-                </Box>
-              : <Box>
-                  <Typography
-                    className={styles.text}
-                    id={'mobilecontent-text'}
-                    sx={
-                      {
-                        fontFamily: demo.font?.label ? `${demo.font?.label} !important` : 'Open Sans'
-                      }
-                    }
-                  >
-                    {demo.language?.value === 'EN' ? 'Select a payment method' : 'Seleccione un método'}
-                  </Typography>
-                </Box>
-          }
+                  {demo.language?.value === 'EN' ? 'Receipt 123' : 'Recibo 123'}
+                </Typography>
+              </Box>
+            </Box>
+          ) : (
+            <Box>
+              <Typography
+                className={styles.text}
+                id={'mobilecontent-text'}
+                sx={{
+                  fontFamily: demo.font?.label
+                    ? `${demo.font?.label} !important`
+                    : 'Open Sans'
+                }}
+              >
+                {demo.language?.value === 'EN'
+                  ? 'Select a payment method'
+                  : 'Seleccione un método'}
+              </Typography>
+            </Box>
+          )}
           <Box>
             <Typography
               id={'mobilecontent-amount'}
-              sx={
-                {
-                  fontWeigth: '700 !important',
-                  color: getCssVar('--demo-mobile-gray-d'),
-                  fontFamily: demo.font?.label ? `${demo.font?.label} !important` : 'Open Sans'
-                }
-              }
+              sx={{
+                fontWeigth: '700 !important',
+                color: getCssVar('--demo-mobile-gray-d'),
+                fontFamily: demo.font?.label
+                  ? `${demo.font?.label} !important`
+                  : 'Open Sans'
+              }}
             >
-              {`${demo.currency?.value ? demo.currency?.value : '$'}55,57 ${demo.currency?.value === '$' ? 'USD' : 'EUR'}`}
+              {`${demo.currency?.value ? demo.currency?.value : '$'}55,57 ${
+                demo.currency?.value === '$' ? 'USD' : 'EUR'
+              }`}
             </Typography>
             <Typography
               id={'mobilecontent-currency'}
-              sx={
-                {
-                  fontWeigth: '400 !important',
-                  color: getCssVar('--primary-buttons'),
-                  fontFamily: demo.font?.label ? `${demo.font?.label} !important` : 'Open Sans'
-                }
-              }
+              sx={{
+                fontWeigth: '400 !important',
+                color: getCssVar('--primary-buttons'),
+                fontFamily: demo.font?.label
+                  ? `${demo.font?.label} !important`
+                  : 'Open Sans'
+              }}
             >
-              {demo.language?.value === 'EN' ? 'Change currency' : 'Cambiar moneda'}
+              {demo.language?.value === 'EN'
+                ? 'Change currency'
+                : 'Cambiar moneda'}
             </Typography>
           </Box>
         </Box>
-        <Box
-          className={styles['custom-payment']}
-        >
+        <Box className={styles['custom-payment']}>
           <Box
             className={styles['arrow-box']}
             id={'mobilecontent-arrow-box'}
-            sx={
-              {
-                top: '-10px',
-                left: '48%'
-              }
-            }
+            sx={{
+              top: '-10px',
+              left: '48%'
+            }}
           >
             <KeyboardArrowUpIcon
               id={'mobilecontent-arrow'}
@@ -172,35 +165,31 @@ const MobileContent = ({ type, onLoad }: Props): JSX.Element => {
             />
           </Box>
           <Box
-            sx={
-              {
-                width: '100%',
-                display: 'flex',
-                overflow: 'hidden',
-                paddingLeft: '8px'
-              }
-            }
+            sx={{
+              width: '100%',
+              display: 'flex',
+              overflow: 'hidden',
+              paddingLeft: '8px'
+            }}
           >
-            <PayCard type='card' />
-            <PayCard type='paypal' />
+            <PayCard type="card" />
+            <PayCard type="paypal" />
           </Box>
           <Box
-            sx={
-              {
-                padding: '0 10px'
-              }
-            }
+            sx={{
+              padding: '0 10px'
+            }}
           >
             <Button
               className={styles.button}
               id={'mobilecontent-button'}
-              sx={
-                {
-                  backgroundColor: `#${demo.button} !important`,
-                  color: `${getCssVar('--white')} !important`,
-                  fontFamily: demo.font?.label ? `${demo.font?.label} !important` : 'Open Sans'
-                }
-              }
+              sx={{
+                backgroundColor: `#${demo.button} !important`,
+                color: `${getCssVar('--white')} !important`,
+                fontFamily: demo.font?.label
+                  ? `${demo.font?.label} !important`
+                  : 'Open Sans'
+              }}
             >
               {demo.language?.value === 'EN' ? 'Pay' : 'Pagar'}
             </Button>
